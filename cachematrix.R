b37c319ba352b402c8cbdd59a909249d7cb12de8
achimt <atreumann@gmail.com>
2014-12-18 07:32:22 +0000

    this is the first version of this OO exercise in the 2nd week of the coursera course. Both functions have been written with the help of Bill Hilton's tutorial (reposted by Pavel Kirjanas in the forum, here https://class.coursera.org/rprog-016/forum/thread?thread_id=96.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve calculate the inverse of an
+## invertible matrix. The matrix is stored as an object with
+## its inverse as a property (voiding the necessity of calculating)
+## the inverse every time it is needed
 
-## Write a short comment describing this function
+## makeCacheMatrix sets up the object, with the inverse as
+## one of its properties and setter and getter functions
+## as its methods. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL # m is the transpose matrix and it is reset to NULL
+               # every time makeCacheMatrix is called
+     set <- function(y) {     # set receives a matrix
+          x <<- y             # and puts it into the 'external' x
+          m <<- NULL          # its inverse is set to NULL
+     }         # The set function 
+     get <- function(){x} # get returns the matrix x as it was
+     set_inverse <- function(solve){m <<- solve}
+               # cacheSolve calls this when it is called for the
+               # first time. The superassignment vector makes sure
+               # that the resulting matrix is stored in m
+     get_inverse <- function() {m}
+               # once m exists, this will return the inverse matrix
+               # when cacheSolve is called
+     list(get = get,
+          set_inverse = set_inverse
+          get_inverse = get_inverse)
+               # this is, I think, a list of the methods in this
+               # class. It helps calling functions to access the
+               # methods. 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve performs the actual calculation, if that has not
+## been done before. In the latter case, it recalls the inverse
+## matrix that has been stored as one of the object's properties. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {   # x is set by makeCacheMatrix
+                                   # as x is an object, it also has
+                                   # methods that can be called. 
+     m <- x$get_inverse()          # this gets the inverse matrix, 
+                                   # either from the cache or it will
+                                   # be calculated. 
+     if(!is.null(m)){              # this is only done, if we already 
+                                   # have a value in m
+          message("getting cached data")
+          return(m)                # the "return" command ends the 
+                                   # cacheSolve() function and returns the value
+     }
+     mat <- x$get()                # if m is NULL, we arrive here
+                                   # and use the get method to get our matrix
+     m <- solve(mat)               # we then fill m with the inverse matrix
+     x$set_inverse(m)              # and we set it as a property of the object x
+     m                             # the last value called by a function is always 
+                                   # returned as the result
 }
